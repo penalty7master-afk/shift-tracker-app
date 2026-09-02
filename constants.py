e7875e0486622b7d3c33971f83c692b17b04db4c
penalty7master-afk <penalty7master@gmail.com>
2026-09-02 15:10:56 +0300

    Add theme accents and arrival options constants

---
+++ constants.py
@@ -0,0 +1,7 @@
+THEME_ACCENTS = {
+    "Aurora Violet": "#c9a6ff",
+    "Midnight Blue": "#7dd3fc",
+    "Emerald Mint": "#6ee7b7",
+}
+
+ARRIVAL_OPTIONS = ["До 20:00", "Буфер (до 20:15)", "Опоздание (до 20:30)"]
